ec3cb444df037ba821c43e7ef06505bbbd5b3ad4
Oleksii Klimov <OleksiiKlimov@users.noreply.github.com>
2019-04-04 13:38:50 +0300

    fixed if's

--- dz6.js
+++ dz6.js
@@ -1,10 +1,10 @@
 //Задача 1. Дано масив цілих чисел. Кожне від’ємне число замінити на 0.
 
 let ar = [-3, 5, 2, -5]
-ar.forEach((x, i, arr) => (arr[i] < 0) ? arr[i] = 0 : arr[i])
+ar.forEach((x, i, arr) => { if (arr[i] < 0) arr[i] = 0 })
 document.write(ar)
 
-//Задача 2. Дано масив цін. Сформувати новий масив, який містить тільки ті ціни, які менші за задану.
+// //Задача 2. Дано масив цін. Сформувати новий масив, який містить тільки ті ціни, які менші за задану.
 const Cina = [1001, 158, 5418, 49, 45, 54, 4, 544, 848, 85, 8, 900, 1200, 5000, 20, 1800]
 const lowCost = Cina.filter(x => x < 50)
 document.write(lowCost)
@@ -13,7 +13,10 @@ document.write(lowCost)
 //для тих, які більші за 1000 грн. відняти 30%.
 
 let arrCina = [1001, 900, 1200, 5000, 20, 1800]
-arrCina.forEach((x, i, arr) => { (arr[i] >= 1000) ? (arr[i] *= 0.7) : (arr[i] *= 1.2) })
+arrCina.forEach((x, i, arr) => {
+    if (arr[i] >= 1000) (arr[i] *= 0.7)
+    if (arr[i] < 1000) (arr[i] *= 1.2)
+})
 document.write(arrCina)
 
 //Задача 4. Дано масив імен. Сформувати новий масив, який складається з імен, які починаються на задану літеру.
@@ -25,13 +28,19 @@ document.write(firstLetter)
 // Задача 5. Дано ваги автомобілів на парковці. З’ясувати, чи усі автомобілі є легковими (легшими за 2000 кг).
 
 const auto = [54164, 654, 5415, 1548, 14814, 1841, 1481, 1048, 1484, 1848, 849]
-auto.every(x => x > 2000) ? document.write('every cars are hevier than 2t.') : document.write('NOT every cars are hevier than 2t.')
+auto.every(x => {
+    if (x > 2000) return ('every cars are hevier than 2t.')
+    if (x < 2000) return ('NOT every cars are hevier than 2t.')
+})
 
 //Задача 6. Дано масив, який зберігає кількість відвідувачів протягом тижня.
 // З’ясувати, чи були дні, коли кількість відвідувачів була меншою за 10.
 
 const client = [54, 54, 15, 79, 12, 545, 45, 94, 45, 456, 45]
-client.some(x => x < 10) ? document.write('it was a few days when clients quatity was less than 10') : document.write('every day clients quatity was more than 10')
+client.some(x => {
+    if (x < 10) return ('it was a few days when clients quatity was less than 10')
+    if (x < 10) return ('every day clients quatity was more than 10')
+})
 
 //Задача 7. Розробити функцію, яка би для довільної кількості переданих символів визначала, кількість букв «а».
 
@@ -48,8 +57,8 @@ document.write(massDob)
 //Задача 9. Розробити функцію, яка би для довільної кількості чисел знаходила максимальне.
 
 const maxArr = [540, 54, 584, 6486, 21, 8641, 8654, 99186, 5418, 61, 8641, 8]
-let max = maxArr.reduce(((acc, x, i, arr) => x > acc ? acc = x : acc), 0)
-document.write(max)
+let maxCount = maxArr.reduce(((max, x, i, arr) => x > max ? max = x : max), 0)
+document.write(maxCount)
 
 //Задача. Відсортувати масив імен за останньою літерою.
 const Name = ['івана', 'петрос', 'сашаб', 'игорьв']
@@ -64,7 +73,10 @@ document.write(lastLetter)
 // (помножити на 0.8), а для усіх інших товарів – надати 5 % знижки (помножити на 0.95).
 
 const arrCin = [100, 1001]
-arrCin.forEach((x, i, arr) => (arr[i] > 1000) ? (arr[i] *= 0.8) : (arr[i] *= 0.95))
+arrCin.forEach((x, i, arr) => {
+    if (arr[i] > 1000) (arr[i] *= 0.8)
+    if (arr[i] < 1000) (arr[i] *= 0.95)
+})
 document.write(arrCin)
 
 //3.	Задача. Дано масив років народження працівників. Вивести масив тих, які є призовного віку (менше 25 років).
@@ -87,7 +99,9 @@ document.write(lowPrice)
 // //Задача. Дано масив цін товарів. Знайти сумарну вартість тих, які знаходяться в межах від 1000 до 2000.
 
 const arrSum = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const sumArr = arrSum.reduce(((acc, x) => 1000 < x < 2000 ? acc += x : acc), 0)
+const sumArr = arrSum.reduce(((acc, x) => {
+    if (1000 < x < 2000) acc += x
+}), 0)
 document.write(sumArr)
 
 // 7.	Задача. Дано масив номерів автомобілів. Сформувати масив тих, які починаються на літеру «А»
